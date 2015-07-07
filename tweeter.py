5702eeb0725ddaf27a5c809cfbb8867df22c4128
Tim Reilly <timreilly@live.com>
2015-07-07 16:47:35 -0700

    tweeter added receiving 401 errors

--- tweeter.py
+++ tweeter.py
@@ -5,6 +5,9 @@
 # twitter client
 import tweepy
 
+#tokens
+from tokens import *
+
 # database interface
 import sqlite3
 conn = sqlite3.connect('tweets.db')
@@ -31,10 +34,7 @@ class StreamWatcherHandler(tweepy.StreamListener):
 
             # Now that we have our tweet information, let's stow it away in our 
             # sqlite database
-            curs.execute("insert into tweets (tid, username, created_at, 
-                          content, reply_to, coordinates, source)        
-                          values(?, ?, ?, ?, ?, ?, ?)",                  
-                          (tid, usr, cat, txt, in_reply_to, coord, src))
+            curs.execute("insert into tweets (tid, username, created_at, content, reply_to, coordinates, source) values(?, ?, ?, ?, ?, ?, ?)", (tid, usr, cat, txt, in_reply_to, coord, src))
             conn.commit()
         except Exception as e:
             # Most errors we're going to see relate to the handling of UTF-8 messages (sorry)
@@ -46,8 +46,8 @@ class StreamWatcherHandler(tweepy.StreamListener):
 
 def main():
     # establish stream
-    consumer_key = "CONSUMER_KEY"
-    consumer_secret = "CONSUMER_SECRET"
+    consumer_key = CONSUMER_KEY
+    consumer_secret = CONSUMER_SECRET
     auth1 = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
 
     access_token = "ACCESS_TOKEN"
