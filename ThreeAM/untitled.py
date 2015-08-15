8cde3c63d825570c7def7aa06c01cc120d0dd51e
Tim Reilly <timreilly@live.com>
2015-08-15 08:04:29 -0700

    Eight AM

--- ThreeAM/untitled.py
+++ ThreeAM/untitled.py
@@ -1,19 +1,37 @@
-def foo(bar, result, index):
-    print 'hello {0}'.format(bar)
-    result[index] = "foo"
+import time
+
+def foo(color, result, index):
+    print 'hello {0}'.format(color)
+    result[index] = time.time()
+
+def blink(color, hue, time, index, wait):
+	time[index] = time.time()
+	color[index] = hue 
+	
 
 from threading import Thread
 
-threads = [None] * 10
-results = [None] * 10
+threads = [None] * 2
+results = [None] * 2
+
+print threads
 
 for i in range(len(threads)):
-    threads[i] = Thread(target=foo, args=('world!', results, i))
+    threads[i] = Thread(target=foo, args=("blue", results, i))
     threads[i].start()
 
 # do some other stuff
 
+colors = [None] * 2
+hues = [None] * 2
+
+
+
+for j in range(len(threads))
+
 for i in range(len(threads)):
     threads[i].join()
 
-print " ".join(results)  # what sound does a metasyntactic locomotive make?
+print results
+
+#print " ".join(results)  # what sound does a metasyntactic locomotive make?
